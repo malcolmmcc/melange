c7e30424ff00fc505a2aebb861f02d51c8fa5c2f
Malcolm McCulloch <malcolm@malcolmm.cc>
2014-03-03 17:02:08 -0500

    use element.value for wordcount for textareas etc

--- wordcount.js
+++ wordcount.js
@@ -2,15 +2,15 @@
    in an arbitrary element on any webpage, by clicking on that element.
 
    How to use: Make a new bookmarket, with "WC" or "wordcount" as the Name and 
-   the following block as the URL. Then, click on that bookmark, then an element
+   the following line/block as the URL. Then, click on that bookmark, then an element
    on any site.
 
-javascript:(function(){document.styleSheets[0].addRule(".highlighted_to_wordcount","background:blue !important;color:white !important");var e=function(e){if(e.keyCode==27){i()}};document.addEventListener("keydown",e);var t=function(e){e.stopPropagation();this.classList.add("highlighted_to_wordcount");return false};var n=function(e){e.stopPropagation();this.classList.remove("highlighted_to_wordcount");return false};var wc=function(e){alert(this.innerText.split(" ").length);i();e.preventDefault();e.stopPropagation();return false};var i=function(){var i=0;var s=document;while(s=document.body.getElementsByTagName("*").item(i++)){s.removeEventListener("mouseover",t);s.removeEventListener("mouseout",n);s.removeEventListener("click",wc);s.classList.remove("highlighted_to_wordcount")}document.removeEventListener("keydown",e)};var s=0;var o=document;while(o=document.body.getElementsByTagName("*").item(s++)){o.addEventListener("mouseover",t);o.addEventListener("mouseout",n);o.addEventListener("click",wc)}})()
+javascript:(function(){document.styleSheets[0].addRule(".highlighted_to_wordcount","background:blue !important;color:white !important");var e=function(e){if(e.keyCode==27){i()}};document.addEventListener("keydown",e);var t=function(e){e.stopPropagation();this.classList.add("highlighted_to_wordcount");return false};var n=function(e){e.stopPropagation();this.classList.remove("highlighted_to_wordcount");return false};var wc=function(e){alert((this.innerText||this.value).split(" ").length);i();e.preventDefault();e.stopPropagation();return false};var i=function(){var i=0;var s=document;while(s=document.body.getElementsByTagName("*").item(i++)){s.removeEventListener("mouseover",t);s.removeEventListener("mouseout",n);s.removeEventListener("click",wc);s.classList.remove("highlighted_to_wordcount")}document.removeEventListener("keydown",e)};var s=0;var o=document;while(o=document.body.getElementsByTagName("*").item(s++)){o.addEventListener("mouseover",t);o.addEventListener("mouseout",n);o.addEventListener("click",wc)}})()
 
    Note that the code below is just so you can see what it does, and to make it
    easier for you to adapt it for your own purposes.
 
-   This is just a tiny modification to code by Christopher Fredericks for DELETING
+   This is just a slight modification to code by Christopher Fredericks for DELETING
    parts of a webpage. His code can be found at
    https://plus.google.com/114437281866675781856/posts/hkp9F9Wr9tQ
 
@@ -38,7 +38,7 @@ var formatted = function () {
         return false
     };
     var wc = function (e) {
-        alert(this.innerText.split(" ").length); // the only meaningful line I changed
+        alert((this.innerText || this.value).split(" ").length); // the only meaningful line I changed
         i();
         e.preventDefault();
         e.stopPropagation();
