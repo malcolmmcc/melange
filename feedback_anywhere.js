eb8c485e8b6f1433c0281d959cb6af08e2635abe
Malcolm Ocean <malcolm.m.ocean@gmail.com>
2014-10-26 18:23:42 -0400

    make the oneliner 100% width using the stylesheet

--- feedback_anywhere.js
+++ feedback_anywhere.js
@@ -1,17 +1,18 @@
 var scripts = document.getElementsByTagName("script");
-var script = scripts[scripts.length - 1];
-var scr = script;
+var scr = scripts[scripts.length - 1];
 
 var emailAddress = scr.getAttribute("data-email");
 var includeRating = !scr.getAttribute("data-hide-rating");
 var question = scr.getAttribute("data-question") || "What was this post about?";
+var title = scr.getAttribute("data-title") || document.title;
 
 var randId = Math.floor(10000000*Math.random());
 var currentRating = 0;
 function sendFeedback(rid) {
   var mailto = "mailto: " + emailAddress;
-  mailto += '?subject=Blog post feedback for "' + document.title + '"';
-  mailto += "&body=Rating: "
+  mailto += '?subject=Blog post feedback for "' + title + '"';
+  mailto += "&body=" + location.href + "%0A%0A";
+  mailto += "Rating: "
   mailto += (new Array(currentRating+1).join("\u2605"));
   mailto += (new Array(6-currentRating).join("\u2606"));
   mailto += "%0A%0A" + question + "%0A" + document.getElementById("feedback_oneliner_" + rid).value;
@@ -33,20 +34,21 @@ if (includeRating) {
     element.classList.add("selected");
   }
 
+  var css = ".feedback_oneliner {width: 100%}";
   if (!document.getElementById("feedback_stars")) {
-    var css = ".feedback_star_rating {width: 100px;text-align:left;display: inline-block;unicode-bidi: bidi-override;direction: rtl}"
+    css += ".feedback_star_rating {width: 100px;text-align:left;display: inline-block;unicode-bidi: bidi-override;direction: rtl}"
     css += ".feedback_star_rating > span {display: inline-block;position: relative;width: 1.1em;}"
     css += ".feedback_star_rating > span:focus {color: #0c0; outline: none;}"
     css += ".feedback_star_rating > span:hover:before, .feedback_star_rating > span:hover ~ span:before, .feedback_star_rating > span.selected:before, .feedback_star_rating > span.selected ~ span:before {content: '\u2605';position: absolute;}"
-    var sheet = document.createElement('style')
-    sheet.appendChild(document.createTextNode(""));
-    sheet.id = "feedback_stars";
-    sheet.innerHTML = css;
-    document.body.appendChild(sheet);
   }
+  var sheet = document.createElement('style')
+  sheet.appendChild(document.createTextNode(""));
+  sheet.id = "feedback_stars";
+  sheet.innerHTML = css;
+  document.body.appendChild(sheet);
 }
 
-var html = '<input type="text" id="feedback_oneliner_' + randId + '" tabindex="100" />';
+var html = '<input type="text" class="feedback_oneliner" placeholder="...type here..." id="feedback_oneliner_' + randId + '" tabindex="100" />';
 
 if (includeRating) {
   html += '&nbsp;Rating: <div class="feedback_star_rating">';
